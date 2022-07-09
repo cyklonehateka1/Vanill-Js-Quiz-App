dd70383da7c57dc5eeaf7ed34e825a00cc6830c0
Emmanuel Hateka <emmanuelhateka.com>
2022-07-09 17:53:54 +0000

    finished project for now, next is deployment

---
+++ js/highscores.js
@@ -0,0 +1,8 @@
+const highScoresList = document.querySelector("#highScoresList");
+const highScore = JSON.parse(localStorage.getItem("highScores")) || [];
+
+highScoresList.innerHTML = highScore
+  .map((score) => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
